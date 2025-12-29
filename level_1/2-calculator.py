307c7782df9032b86586942c6976d68f067bfac5
ryan woodward <ryanjwoodward@outlook.com>
2025-12-29 12:15:16 -0700

    Renamed challenge 1 and added challenge 2

---
+++ level_1/2-calculator.py
@@ -0,0 +1,21 @@
+# Challenge: Using the REPL perform arithmetic operations (+, -, /, *)
+# NOTE: this requires the use of Micropy REPL (i.e. via Picocom)
+
+# Addition
+sum = 5 + 3
+print(sum)
+
+# Subtraction
+diff = 5 - 3
+print(diff)
+
+# Division
+quot = 10 / 2
+print(quot)
+
+# Multiplication
+prod = 5 * 2
+print(prod)# NOTE: this requires the use of Micropy REPL (i.e. via Picocom)
+
+# Put some operations together
+print(5 + 2 * 3)
